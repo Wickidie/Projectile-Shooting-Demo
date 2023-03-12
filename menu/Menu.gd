566617e6ccc25c5ff9570bbe38de1f95d08b35ad
Wickidie <121784189+Wickidie@users.noreply.github.com>
2023-03-12 09:56:49 +0700

    add menu

---
+++ menu/Menu.gd
@@ -0,0 +1,51 @@
+extends NinePatchRect
+
+@export var arena_scene: PackedScene
+signal button_pressed
+
+func _ready():
+	
+	pass
+
+func _on_play_button_pressed():
+	
+	print($ButtonContainer/PlayButton)
+	pass # Replace with function body.
+
+func _on_option_button_pressed():
+	print($ButtonContainer/OptionButton)	
+	pass # Replace with function body.
+
+func _on_exit_button_pressed():
+	print($ButtonContainer/ExitButton)	
+	pass # Replace with function body.
+
+
+func _on_play_button_mouse_entered():
+	$ButtonContainer/PlayButton.modulate = Color(0.8, 0.8, 0.8)
+	pass # Replace with function body.
+
+
+func _on_option_button_mouse_entered():
+	$ButtonContainer/OptionButton.modulate = Color(0.8, 0.8, 0.8)
+	pass # Replace with function body.
+
+
+func _on_exit_button_mouse_entered():
+	$ButtonContainer/ExitButton.modulate = Color(0.8, 0.8, 0.8)
+	pass # Replace with function body.
+
+
+func _on_play_button_mouse_exited():
+	$ButtonContainer/PlayButton.modulate = Color(1.0, 1.0, 1.0)
+	pass # Replace with function body.
+
+
+func _on_option_button_mouse_exited():
+	$ButtonContainer/OptionButton.modulate = Color(1.0, 1.0, 1.0)
+	pass # Replace with function body.
+
+
+func _on_exit_button_mouse_exited():
+	$ButtonContainer/ExitButton.modulate = Color(1.0, 1.0, 1.0)
+	pass # Replace with function body.
